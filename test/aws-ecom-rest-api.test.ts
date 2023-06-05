eca8e4cca82516dee21bfbee86ba13beeb677675
Sreenath Ramanna <sreenathr@pop-os.localdomain>
2023-06-05 19:05:50 +1200

    Initial commit

---
+++ test/aws-ecom-rest-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEcomRestApi from '../lib/aws-ecom-rest-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ecom-rest-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEcomRestApi.AwsEcomRestApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
